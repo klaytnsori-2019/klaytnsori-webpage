fd80f187d7fc09b2100c35070a59e7a7aadd9299
eddie-kwon <eddie-kwon@key-inside.com>
2019-08-05 15:42:40 +0900

    add SamplePage

---
+++ src/page/SamplePage.vue
@@ -0,0 +1,47 @@
+<template>
+    <!-- route.js에 해당 컴포넌트를 등록해야 컴포넌트를 띄울 수 있습니다. -->
+    <!-- route에 관한 내용은 route.js를 참고하세요. -->
+
+    <div id="sample_page">
+        <!-- components에 등록한 컴포넌트를 화면에 그립니다. -->
+        <header/>
+        <div class="content_area" v-on:id="divId">
+            여기에 컨텐츠 내용이 들어갑니다.
+            {{ variable }}
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './../component/common/Header'
+
+    export default {
+        name: "login_page",
+        components: { Header },
+        data:() => {
+            return {
+                //이곳에 필요한 변수들을 넣습니다.
+                variable: "변수에 들어간 내용입니다.",
+                divId: "content_div_id"
+            }
+        },
+        methods: {
+            //이곳에 템플릿에서 참조할 함수를 작성합니다.
+        }
+    }
+
+    // 이곳에 일반 함수들을 작성합니다.
+</script>
+
+<style scoped>
+    /* 여기에 css요소를 등록합니다.
+     * style 뒤에 scoped를 붙이면 컴포넌트 내부에만 적용됩니다.
+     * 공통으로 적용할 style이 있다면 scoped를 빼던지, App.vue에 작성합니다.  */
+    .content_area {
+        background: white;
+        display: table;
+        width: 100%;
+        height: -webkit-fill-available;
+        text-align: center;
+    }
+</style>
--- src/route.js
+++ src/route.js
@@ -4,13 +4,18 @@ import VueRouter from 'vue-router'
 import LoginPage from './page/LoginPage.vue'
 import MainPage from './page/MainPage.vue'
 
+// 컴포넌트를 import하고, router에 경로를 등록합니다.
+// SamplePage는 localhost:3000/sample 등으로 접속 가능합니다.
+import SamplePage from './page/SamplePage.vue'
+
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
         { path: '/login', component: LoginPage },
-        { path: '/main', component: MainPage }
+        { path: '/main', component: MainPage },
+        { path: '/sample', component: SamplePage}
     ]
 });
 
