7986372895b0a9f92f8d9a9956e9ec7f043cd194
doyun0916 <52279390+doyun0916@users.noreply.github.com>
2019-08-11 13:16:52 +0900

    질문보기(detail) 페이지 작성! QuestionDetail은 따로 router-link :to 설정 안했음.

---
+++ src/component/main/QuestionDetailATile.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="answer_list_tile">
+      <div class="id">{{id}}</div>
+      <div class="detail">{{detail}}</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "QuestionDetailATile",
+    props: [
+      "detail",
+      "index",
+      "id"
+    ]
+  }
+</script>
+
+<style scoped>
+  .answer_list_tile {
+    margin: 10px auto;
+    width: 861px;
+    height: 150px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .detail{
+    width: 800px;
+    height: 48px;
+    font-family: NanumGothic;
+    font-size: 14px;
+    font-weight: normal;
+    text-align: left;
+    color: #9b9b9b;
+    margin-top: 40px;
+    margin-left:25px;
+    white-space:pre;
+  }
+
+  .id{
+    width: 95px;
+    height: 14px;
+    font-family: NanumGothic;
+    font-size: 12px;
+    font-weight: normal;
+    text-align: left;
+    margin-top: 15px;
+    margin-left: 25px;
+    position:absolute;
+    color: #656565;
+    white-space:pre;
+  }
+</style>
---
+++ src/component/main/QuestionDetailQTile.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="main_list_tile">
+    <div class="id">{{id}}</div>
+    <div class="title">{{title}}</div>
+    <span class="date">{{date}}</span>
+    <span class="reward">보상 : {{reward}} klay</span>
+    <div class="detail">{{detail}}</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "QuestionDetailQTile",
+    props: [
+      "title",
+      "detail",
+      "reward",
+      "index",
+      "date",
+      "id"
+    ]
+  }
+</script>
+
+<style scoped>
+  .main_list_tile {
+    margin: 10px auto;
+    width: 861px;
+    height: 350px;
+    border-radius: 10px;
+    background-color: #ffffff;
+    display: flex;
+    justify-content: space-between;
+  }
+  .title {
+    width: 296px;
+    height: 17px;
+    font-family: NanumGothic;
+    font-size: 15px;
+    font-weight: bold;
+    text-align: left;
+    color: #717171;
+    white-space:pre;
+    margin-top:45px;
+    margin-left:25px;
+  }
+
+  .detail{
+    width: 800px;
+    height: 48px;
+    font-family: NanumGothic;
+    font-size: 14px;
+    font-weight: normal;
+    text-align: left;
+    color: #9b9b9b;
+    margin-top: 150px;
+    margin-left:25px;
+    white-space:pre;
+    position:absolute;
+  }
+
+  .reward {
+    font-family: NanumGothic;
+    font-size: 12px;
+    font-weight: bold;
+    line-height: 1.17;
+    text-align: right;
+    color: #5d5d5d;
+    width: 110px;
+    white-space:pre;
+    margin-top: 60px;
+    margin-right:20px;
+  }
+  .id{
+    width: 95px;
+    height: 14px;
+    font-family: NanumGothic;
+    font-size: 12px;
+    font-weight: normal;
+    text-align: left;
+    margin-top: 15px;
+    margin-left: 25px;
+    position:absolute;
+    color: #656565;
+    white-space:pre;
+  }
+  .date{
+    font-family: NanumGothic;
+    font-size: 10px;
+    font-weight: normal;
+    text-align: left;
+    margin-top: 70px;
+    margin-left:25px;
+    color: #656565;
+    position:absolute;
+  }
+</style>
