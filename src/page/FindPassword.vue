47d5d0cbd84a1ddec787ccb2356c647df81e53a7
doyun0916 <doyun0916@naver.com>
2019-08-29 15:54:59 +0900

    비밀번호찾기 e-mail입력페이지까지 완료! LoginForm에 router-link 설정했음.

---
+++ src/page/FindPassword.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="login_page">
+    <div class="content_area">
+      <FindForm/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import FindForm from './../component/password/FindForm.vue'
+
+  export default {
+    name: "FindPassword",
+    components: { FindForm }
+  }
+</script>
+
+<style scoped>
+  .content_area {
+    background: white;
+    display: table;
+    width: 100%;
+    height: -webkit-fill-available;
+    text-align: center;
+  }
+</style>
+
