45971e8f4117e6e225a977d5f635a152fdf3e104
yyoongs <jysid3531@gmail.com>
2019-09-05 23:58:18 +0900

    마이페이지 추가

---
+++ src/page/MyKlay.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="main_page" class="page">
+    <Header></Header>
+    <div class="contents_area">
+      <wallet_info></wallet_info>
+      <div id="menu_name">
+        <span id="menu">My klay</span>
+        <div id="contents">
+          <ul>
+            <li v-for="item in like_item">
+              <klay-list-tile :buys="item.buy" :sells="item.sell" :title="item.title" :reward="item.reward"></klay-list-tile>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import Header from "../component/common/Header";
+    import wallet_info from "../component/wallet/wallet_info";
+    import KlayListTile from "../component/klay/KlayListTile";
+
+    export default {
+        name: "MyKlay",
+        components:{Header,wallet_info,KlayListTile},
+        data: () => {
+            return {
+                like_item: like_item
+            }
+        }
+    }
+
+    let like_item = [
+        {title: "이런이런 질문이 있습니다! 답변 부탁드립니다", reward: 10, sell:true},
+        {title: "이런이런 질문이 있습니다! 답변 부탁드립니다", reward: 20, sell:false},
+        {title: "이런이런 질문이 있습니다! 답변 부탁드립니다", reward: 30, sell:false},
+        {title: "이런이런 질문이 있습니다! 답변 부탁드립니다", reward: 40, sell:false},
+        {title: "이런이런 질문이 있습니다! 답변 부탁드립니다", reward: 50}
+
+    ];
+
+</script>
+
+<style scoped>
+
+  .contents_area {
+    margin: auto;
+    /*position: relative;*/
+    text-align: center;
+    width:1000px;
+  }
+
+
+  #menu {
+    font-family: "Nanum Gothic";
+    font-size: 20px;
+    color: #5d5d5d;
+    margin-right: 790px;
+    margin-top: 20px;
+  }
+
+  #contents {
+    margin : 20px auto;
+  }
+
+  #addd .reward {
+    color : blue;
+  }
+
+  #subb .reward {
+    color : red;
+  }
+
+
+</style>
--- src/route.js
+++ src/route.js
@@ -6,10 +6,13 @@ import LoginPage from './page/LoginPage.vue'
 import MainPage from './page/MainPage.vue'
 import QuestionDetail from './page/QuestionDetail.vue'
 import QuestionPage from "./page/QuestionPage";
-import MyPage from "./page/MyPage";
+import MyKlay from "./page/MyKlay";
+import MyAnswer from "./page/MyAnswer";
+import MyQuestion from "./page/MyQuestion";
 // 컴포넌트를 import하고, router에 경로를 등록합니다.
 // SamplePage는 localhost:3000/sample 등으로 접속 가능합니다.
 import SamplePage from './page/SamplePage.vue'
+import MyPage from "./page/MyPage";
 
 Vue.use(VueRouter);
 
@@ -21,8 +24,11 @@ const router = new VueRouter({
         { path: '/sample', component: SamplePage},
         { path: '/question_list', component: QuestionList},
         { path: '/detail', component:QuestionDetail},
-        { path: '/question', component:QuestionPage},
-        { path: '/mypage',component:MyPage}
+      {path: '/mypage', component:MyPage},
+      {path: '/question', component:QuestionPage},
+      {path:'/myklay', component:MyKlay},
+      {path:'/myquestion', component:MyQuestion},
+      {path:'/myanswer', component:MyAnswer}
         ]
 });
 
