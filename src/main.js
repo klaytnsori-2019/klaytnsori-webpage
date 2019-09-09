185ad60e6c17ca119979321fce7125a71bc41141
doyun0916 <doyun0916@naver.com>
2019-09-09 12:20:09 +0900

    index.js에 그냥 변수 storeInput으로 추가 했습니다 기본 store.js 삭제하고!

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './route.js'
-import {store} from './store/store'
+import {store} from './store/index'
 
 Vue.config.productionTip = false;
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,18 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+export const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    storeInput: null
   },
   mutations: {
     increment (state) {
       state.count++
+    },
+    transferString (state, payload){
+      console.log(payload);
+      state.storeInput = payload
     }
   }
 });
